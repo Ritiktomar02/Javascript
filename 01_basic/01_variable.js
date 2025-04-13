b37db4e933ba4f504944d551eda95d72428efb55
Ritik Tomar <ritiktomar@Ritiks-MacBook-Air.local>
2025-04-13 16:14:23 +0530

    new commit

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,20 @@
+// const accountId = 144553;
+// let acountEmail = "ritik0209@gmail.com";
+// var accountPassword = "1234";
+// accountcity = "morena";
+
+// let account; // undefined will be assign
+
+// //accountId=2 //not allowed
+
+// acountEmail = "asdfsd";
+// accountPassword = "3r23";
+// accountcity = "gwalior";
+// console.log(accountId);
+
+// console.table([accountId, acountEmail, accountPassword, accountcity]);
+let n="34ga";
+
+n=Number(n);
+
+console.log(n);
---
+++ 01_basic/02_variable.js
@@ -0,0 +1,69 @@
+// "use strict"; // treat all JS code as newer version
+
+// // alert( 3 + 3) // we are using nodejs, not browser
+
+// console.log(3 
+//     +
+//      3) // code readability should be high
+
+// console.log("Hitesh")
+
+
+// let name = "hitesh"
+// let age = 18
+// let isLoggedIn = false
+// let state;
+
+// // number => 2 to power 53
+// // bigint
+// // string => ""
+// // boolean => true/false
+// // null => standalone value
+// // undefined => 
+// // symbol => unique
+
+
+// // object
+
+// // console.log(typeof undefined); // undefined
+// // console.log(typeof null); // object
+
+// const arr=[1,2,3,4,5];
+
+// const arr2=arr;
+
+// arr2[2]=6;
+
+// console.log(arr2)
+// console.log(arr)
+
+// let text = "How are you doing today?";
+// const myArray = text.split(" ",4);
+// console.log(myArray);
+
+// console.log(Math.random())
+
+// let mydata=new Date();
+
+// console.log(mydata.toLocaleDateString());
+
+// const two=()=>{
+//     const name="ritik";
+
+//     const one=()=>{
+//         console.log(`${name}`);
+//     }
+//     one();
+// }
+
+// two();
+
+let arr=[2,3,4,65,12,45,78];
+
+const ans=arr.filter((nums)=>(nums<10));
+const a=arr.map((nums)=>(nums))
+
+console.log(a)
+
+
+
