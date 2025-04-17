2961cb8e914fdd5db4e4c8bfc4e66b77a3fc2029
Ritik Tomar <ritiktomar@Ritiks-MacBook-Air.local>
2025-04-17 16:22:24 +0530

    new async

---
+++ async/index.js
@@ -0,0 +1,23 @@
+
+
+// const fetchdata=async()=>{
+
+//     try{
+
+//         const response= await fetch('https://randomuser.me/api/')
+//         console.log(typeof(response));
+//         const data=await response.json();
+//         console.log(data)
+
+//     }
+//     catch(error){
+//         console.log("error: ",error);
+
+//     }
+
+// }
+
+// fetchdata();
+
+a=5;
+console.log(typeof(a));
