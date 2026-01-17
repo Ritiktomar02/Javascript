2e9c18e81bef6a63121476f8a85e691c9cf0a9da
Ritik Tomar <ritiktomar0209@gmail.com>
2026-01-17 10:26:35 +0530

    complete eventlistner

--- index.js
+++ index.js
@@ -1,26 +1,8 @@
-let form = document.querySelector(".formtag");
-let fields = document.querySelectorAll("input");
+const text = document.querySelector("input");
+const s = document.querySelector("span");
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  let div = document.createElement("div");
-
-  let p1 = document.createElement("p");
-  p1.textContent = `Name: ${fields[0].value}`;
-
-  let p2 = document.createElement("p");
-  p2.textContent = `Email: ${fields[1].value}`;
-
-  let p3 = document.createElement("p");
-  p3.textContent = `Description: ${fields[2].value}`;
-
-  let p4 = document.createElement("p");
-  p4.textContent = `Age: ${fields[3].value}`;
-
-  div.append(p1, p2, p3, p4);
-  document.body.appendChild(div);
-
-  // Optional: clear form after submit
-  form.reset();
+text.addEventListener("input", (e) => {
+  const left = 20 - text.value.length;
+  s.textContent = left;
+  s.style.color = left < 0 ? "red" : "black";
 });
